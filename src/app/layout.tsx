9db7e74fdbd33df631c592ec94d6fedab7136b69
kolioaris <111509679+kolioaris@users.noreply.github.com>
2025-10-24 23:38:53 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Fira_Code } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ThemeProvider } from "./components/theme-provider";
 
 const Fira = Fira_Code({
