6078eebf6e514e32c19d4d072f2bef8690a5f4dd
kolioaris <111509679+kolioaris@users.noreply.github.com>
2025-10-25 10:40:37 +0300

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,22 +1,10 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://yoursite.com',
-      lastModified: new Date(),
-      changeFrequency: 'yearly',
-      priority: 1,
-    },
-    {
-      url: 'https://yoursite.com/about',
+      url: 'https://kolioaris.xyz',
       lastModified: new Date(),
       changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://yoursite.com/blog',
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
+      priority: 1,
     },
   ]
 }
